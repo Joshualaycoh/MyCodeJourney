d85e9f7a84824bcb5431fe7ed59ea2693e64a28b
Joshualaycoh <your_email@example.com>
2025-05-20 21:24:49 +0800

    modify the header

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -47,7 +47,7 @@ const Header = (isDarkMode) => {
         text-white flex items-center justify-center gap-2 dark:bg-transparent'
         >contact me <Image src={assets.right_arrow_white} 
         alt='' className='w-3 h-3 ' /> </motion.a>
-        <motion.a 
+        {/* <motion.a 
           initial={{y: 30, opacity: 0}}
           whileInView={{y:0, opacity:1}}
           transition={{duration: 0.6, delay:1}}
@@ -55,7 +55,7 @@ const Header = (isDarkMode) => {
         className='px-10 py-2 border rounded-full border-gray-500 flex
         items-center gap-2 bg-white dark:text-black '
         >my resume <Image src={assets.download_icon} 
-        alt='' className='w-4' /> </motion.a>
+        alt='' className='w-4' /> </motion.a> */}
        </div>
     </div>
   )
